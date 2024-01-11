cbe38a292ba8cd9e065732345d1bb16ea783d4bf
Nicola Titmus <nicola.koo@cantab.net>
2024-01-11 10:18:51 +0000

    initial commit

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+
+  backend "s3" {
+    # This backend configuration is filled in automatically at test time by Terratest. If you wish to run this example
+    # manually, uncomment and fill in the config below.
+
+    bucket         = "terraform-book-state-nt2"
+    key            = "ghactions_terraform/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "terraform-book-locks2"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-2"
+}
